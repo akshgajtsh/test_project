907fb40b82ffd9f3093a9bba7f63158026c3ef28
Takayoshi-kobayashi <taka4677@ezweb.ne.jp>
2025-03-29 15:31:54 +0900

    add: relation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,10 @@ class DatabaseSeeder extends Seeder
             [
                 TestSeeder::class,
                 UserSeeder::class,
+                AreaSeeder::class,
+                ShopSeeder::class,
+                RouteSeeder::class,
+                RouteShopSeeder::class
             ]
         );
         \App\Models\ContactForm::factory(100)->create();
